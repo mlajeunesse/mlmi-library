a048d1c40a186212a916c1dd253dd8a89fd76cf4
Mathieu Lajeunesse <contact@mathieulajeunesse.com>
2021-01-19 21:56:27 -0500

    Améliore l'extension BEM

--- plugins/bem.js
+++ plugins/bem.js
@@ -1,53 +1,41 @@
 /*
 *	BlockElement
 */
-$.fn.BlockElement = function(_block_class)
-{
-	var _el = this;
-	_el.block_class = undefined;
+$.fn.BlockElement = function(_mainClass) {
+	let self = this
+	self.mainClass = _mainClass
 
-	/* Get original block class */
-	_el.getBlockClass = function()
-	{
-		return _el.block_class;
-	};
-
-	/* Add an element */
-	_el.addElement = function(_element_class, _element)
-	{
-		if (_element == undefined) _element = "div";
-		var newBlockElement = $('<' + _element + '>').BlockElement(_el.getBlockClass() + "__" + _element_class);
-		return newBlockElement;
-	};
+	/* Add child element */
+	self.addElement = function(elementClass, element) {
+		if (element == undefined) {
+      element = 'div'
+    }
+		var newBlockElement = $('<' + element + '>').BlockElement(self.mainClass + '__' + elementClass)
+		return newBlockElement
+	}
 
 	/* Add a modifier */
-	_el.addModifier = function(_modifier_class)
-	{
-		_el.addClass(_el.block_class + "--" + _modifier_class);
-		return _el;
-	};
+	self.addModifier = function(modifier) {
+		self.addClass(self.mainClass + '--' + modifier)
+		return self
+	}
 
   /* Has a modifier */
-  _el.hasModifier = function(_modifier_class)
-	{
-		return _el.hasClass(_el.block_class + "--" + _modifier_class);
-	};
+  self.hasModifier = function(modifier) {
+		return self.hasClass(self.mainClass + '--' + modifier)
+	}
 
 	/* Remove a modifier */
-	_el.removeModifier = function(_modifier_class)
-	{
-		_el.removeClass(_el.block_class + "--" + _modifier_class);
-		return _el;
-	};
+	self.removeModifier = function(modifier) {
+		self.removeClass(self.mainClass + '--' + modifier)
+		return self
+	}
 
-	/* Initializer */
-	return function()
-	{
-		if (_block_class == undefined){
-			_block_class = _el.attr("class").split(" ")[0];
+	return function() {
+		if (self.mainClass == undefined){
+			self.mainClass = self.attr('class').split(' ')[0]
 		}
-		_el.block_class = _block_class;
-		_el.addClass(_el.block_class)
-		return _el;
-	}();
+		self.addClass(self.mainClass)
+		return self
+	}()
 }
