53a22e39bf14120b3dd256ca599c637bf0be6c18
Mathieu Lajeunesse <contact@mathieulajeunesse.com>
2021-04-09 18:29:00 -0400

    Ajoute des constantes IS_IOS et IS_ANDROID

--- utils/animation.js
+++ utils/animation.js
@@ -1,3 +1,7 @@
 /* Event string shortcuts */
-export const TRANSITION_END = "webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend"
-export const ANIMATION_END = "webkitAnimationEnd oanimationend oAnimationEnd msAnimationEnd animationend"
+export const TRANSITION_END = 'webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend'
+export const ANIMATION_END = 'webkitAnimationEnd oanimationend oAnimationEnd msAnimationEnd animationend'
+
+/* Use with caution */
+export const IS_IOS = navigator.platform && /iPad|iPhone|iPod/.test(navigator.platform)
+export const IS_ANDROID = /(android)/i.test(navigator.userAgent)
